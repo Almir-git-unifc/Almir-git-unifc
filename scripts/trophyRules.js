e9b980d4ff880929e778ade5fb0a509a372502f2
Almir-Git <81040770+Almir-git-unifc@users.noreply.github.com>
2026-02-03 21:58:48 -0300

    Add trophy rules for experience, stars, and PRs
    Create trophyRules.js with rank of tar PR and experience

---
+++ scripts/trophyRules.js
@@ -0,0 +1,32 @@
+export const EXPERIENCE_RULES = [
+  { rank: "SSS", subtitle: "Seasoned Veteran", min: 20 },
+  { rank: "SS",  subtitle: "Grandmaster",      min: 15 },
+  { rank: "S",   subtitle: "Master Dev",       min: 10 },
+  { rank: "AAA", subtitle: "Expert Dev",       min: 7.5 },
+  { rank: "AA",  subtitle: "Experienced Dev",  min: 5 },
+  { rank: "A",   subtitle: "Intermediate Dev", min: 3 },
+  { rank: "B",   subtitle: "Junior Dev",       min: 1.5 },
+  { rank: "C",   subtitle: "Newbie",           min: 0.5 },
+];
+
+export const STAR_RULES = [
+  { rank: "SSS", subtitle: "Super Stargazer", min: 2000 },
+  { rank: "SS",  subtitle: "High Stargazer",  min: 700 },
+  { rank: "S",   subtitle: "Stargazer",       min: 200 },
+  { rank: "AAA", subtitle: "Super Star",      min: 100 },
+  { rank: "AA",  subtitle: "High Star",       min: 50 },
+  { rank: "A",   subtitle: "You are a Star",  min: 30 },
+  { rank: "B",   subtitle: "Middle Star",     min: 10 },
+  { rank: "C",   subtitle: "First Star",      min: 1 },
+];
+
+export const PR_RULES = [
+  { rank: "SSS", subtitle: "God Puller",    min: 1000 },
+  { rank: "SS",  subtitle: "Deep Puller",   min: 500 },
+  { rank: "S",   subtitle: "Super Puller",  min: 200 },
+  { rank: "AAA", subtitle: "Ultra Puller",  min: 100 },
+  { rank: "AA",  subtitle: "Hyper Puller",  min: 50 },
+  { rank: "A",   subtitle: "High Puller",   min: 20 },
+  { rank: "B",   subtitle: "Middle Puller", min: 10 },
+  { rank: "C",   subtitle: "First Pull",    min: 1 },
+];
