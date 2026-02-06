470b73a37a0c16dcfe953c2732803b493e13579d
Almir-Git <81040770+Almir-git-unifc@users.noreply.github.com>
2026-02-06 19:08:36 -0300

    Delete scripts/rank.js
    file without use, and rankByPoints() without called

--- scripts/rank.js
+++
@@ -1,7 +0,0 @@
-export function rankByPoints(points) {
-  if (points >= 200) return "AAA";
-  if (points >= 150) return "AA";
-  if (points >= 100) return "A";
-  if (points >= 50)  return "B";
-  return "C";
-}
