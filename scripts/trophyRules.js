daacbd6f12ddea4857cd2b831fa58dbae0814745
Almir-Git <81040770+Almir-git-unifc@users.noreply.github.com>
2026-02-13 21:05:34 -0300

    Update trophyRules.js
    Update progressbar in star rules, commit rules, pr_rules, repo rules

--- scripts/trophyRules.js
+++ scripts/trophyRules.js
@@ -9,46 +9,50 @@ export const EXPERIENCE_RULES = [
   { rank: "C",   subtitle: "Newbie",           min: 0.5 },
 ];
 
+
 export const STAR_RULES = [
-  { rank: "SSS", subtitle: "Super Stargazer", min: 2000 },
-  { rank: "SS",  subtitle: "High Stargazer",  min: 700 },
-  { rank: "S",   subtitle: "Stargazer",       min: 200 },
-  { rank: "AAA", subtitle: "Super Star",      min: 100 },
-  { rank: "AA",  subtitle: "High Star",       min: 50 },
-  { rank: "A",   subtitle: "You are a Star",  min: 30 },
-  { rank: "B",   subtitle: "Middle Star",     min: 10 },
-  { rank: "C",   subtitle: "First Star",      min: 1 },
+  { rank: "SSS", subtitle: "Super Stargazer", min: 2000, progress: 100 },
+  { rank: "SS",  subtitle: "High Stargazer",  min: 700,  progress: 95 },
+  { rank: "S",   subtitle: "Stargazer",       min: 200,  progress: 85 },
+  { rank: "AAA", subtitle: "Super Star",      min: 100,  progress: 60 },
+  { rank: "AA",  subtitle: "High Star",       min: 50,   progress: 45 },
+  { rank: "A",   subtitle: "You are a Star",  min: 30,   progress: 25 },
+  { rank: "B",   subtitle: "Middle Star",     min: 10,   progress: 10 },
+  { rank: "C",   subtitle: "First Star",      min: 1,    progress: 3 },
 ];
+
 
 export const PR_RULES = [
-  { rank: "SSS", subtitle: "God Puller",    min: 1000 },
-  { rank: "SS",  subtitle: "Deep Puller",   min: 500 },
-  { rank: "S",   subtitle: "Super Puller",  min: 200 },
-  { rank: "AAA", subtitle: "Ultra Puller",  min: 100 },
-  { rank: "AA",  subtitle: "Hyper Puller",  min: 50 },
-  { rank: "A",   subtitle: "High Puller",   min: 20 },
-  { rank: "B",   subtitle: "Middle Puller", min: 10 },
-  { rank: "C",   subtitle: "First Pull",    min: 1 },
+  { rank: "SSS", subtitle: "God Puller",    min: 1000, progress: 100 },
+  { rank: "SS",  subtitle: "Deep Puller",   min: 500,  progress: 95 },
+  { rank: "S",   subtitle: "Super Puller",  min: 200,  progress: 85 },
+  { rank: "AAA", subtitle: "Ultra Puller",  min: 100,  progress: 70 },
+  { rank: "AA",  subtitle: "Hyper Puller",  min: 50,   progress: 30 },
+  { rank: "A",   subtitle: "High Puller",   min: 20,   progress: 20 },
+  { rank: "B",   subtitle: "Middle Puller", min: 10,   progress: 10 },
+  { rank: "C",   subtitle: "First Pull",    min: 1,    progress: 3 },
 ];
+
 
 export const REPO_RULES = [
-  { rank: "SSS", subtitle: "God Repo Creator",    min: 50 },
-  { rank: "SS",  subtitle: "Deep Repo Creator",   min: 45 },
-  { rank: "S",   subtitle: "Super Repo Creator",  min: 40 },
-  { rank: "AAA", subtitle: "Ultra Repo Creator",  min: 35 },
-  { rank: "AA",  subtitle: "Hyper Repo Creator",  min: 30 },
-  { rank: "A",   subtitle: "High Repo Creator",   min: 20 },
-  { rank: "B",   subtitle: "Middle Repo Creator", min: 10 },
-  { rank: "C",   subtitle: "Starter Repo Creator",min: 0 },
+  { rank: "SSS", subtitle: "God Repo Creator",    min: 50, progress: 100 },
+  { rank: "SS",  subtitle: "Deep Repo Creator",   min: 45, progress: 90 },
+  { rank: "S",   subtitle: "Super Repo Creator",  min: 40, progress: 80 },
+  { rank: "AAA", subtitle: "Ultra Repo Creator",  min: 35, progress: 70 },
+  { rank: "AA",  subtitle: "Hyper Repo Creator",  min: 30, progress: 60 },
+  { rank: "A",   subtitle: "High Repo Creator",   min: 20, progress: 40 },
+  { rank: "B",   subtitle: "Middle Repo Creator", min: 10, progress: 20 },
+  { rank: "C",   subtitle: "Starter Repo Creator",min: 0,  progress: 5 },
 ];
 
+
 export const COMMIT_RULES = [
-  { rank: "SSS", subtitle: "Commit God",        min: 5000 },
-  { rank: "SS",  subtitle: "Commit Monster",    min: 3000 },
-  { rank: "S",   subtitle: "Commit Master",     min: 1500 },
-  { rank: "AAA", subtitle: "Commit Expert",     min: 800 },
-  { rank: "AA",  subtitle: "Commit Pro",        min: 400 },
-  { rank: "A",   subtitle: "Committer",         min: 200 },
-  { rank: "B",   subtitle: "Frequent Commit",   min: 50 },
-  { rank: "C",   subtitle: "Getting Started",   min: 1 },
+  { rank: "SSS", subtitle: "Commit God",     min: 5000, progress: 100 },
+  { rank: "SS",  subtitle: "Commit Monster", min: 3000, progress: 95 },
+  { rank: "S",   subtitle: "Commit Master",  min: 1500, progress: 85 },
+  { rank: "AAA", subtitle: "Commit Expert",  min: 800,  progress: 70 },
+  { rank: "AA",  subtitle: "Commit Pro",     min: 400,  progress: 55 },
+  { rank: "A",   subtitle: "Committer",      min: 200,  progress: 40 },
+  { rank: "B",   subtitle: "Frequent Commit",min: 50,   progress: 15 },
+  { rank: "C",   subtitle: "Getting Started",min: 1,    progress: 5 },
 ];
