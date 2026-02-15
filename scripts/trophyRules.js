4053310d3058ba3f3ad7e37318613940425907a6
Almir-Git <81040770+Almir-git-unifc@users.noreply.github.com>
2026-02-14 22:18:48 -0300

    Update trophyRules.js
    update commit_rulles

--- scripts/trophyRules.js
+++ scripts/trophyRules.js
@@ -1,7 +1,7 @@
 export const EXPERIENCE_RULES = [
-  { rank: "SSS", subtitle: "Seasoned Veteran", min: 20 },
-  { rank: "SS",  subtitle: "Grandmaster",      min: 15 },
-  { rank: "S",   subtitle: "Master Dev",       min: 10 },
+  { rank: "SSS", subtitle: "Sr Seasoned Veteran", min: 20 },
+  { rank: "SS",  subtitle: "Sr Grandmaster Dev",  min: 15 },
+  { rank: "S",   subtitle: "Sr Master Dev",       min: 10 },
   { rank: "AAA", subtitle: "Expert Dev",       min: 7.5 },
   { rank: "AA",  subtitle: "Experienced Dev",  min: 5 },
   { rank: "A",   subtitle: "Intermediate Dev", min: 3 },
@@ -47,12 +47,12 @@ export const REPO_RULES = [
 
 
 export const COMMIT_RULES = [
-  { rank: "SSS", subtitle: "Commit God",     min: 5000, progress: 100 },
-  { rank: "SS",  subtitle: "Commit Monster", min: 3000, progress: 95 },
-  { rank: "S",   subtitle: "Commit Master",  min: 1500, progress: 85 },
-  { rank: "AAA", subtitle: "Commit Expert",  min: 800,  progress: 70 },
-  { rank: "AA",  subtitle: "Commit Pro",     min: 400,  progress: 55 },
-  { rank: "A",   subtitle: "Committer",      min: 200,  progress: 40 },
-  { rank: "B",   subtitle: "Frequent Commit",min: 50,   progress: 15 },
-  { rank: "C",   subtitle: "Getting Started",min: 1,    progress: 5 },
+  { rank: "SSS", subtitle: "Commit God",      min: 4000, progress: 100 },
+  { rank: "SS",  subtitle: "Commit Monster",  min: 2000, progress: 95 },
+  { rank: "S",   subtitle: "Ultra Committer", min: 1000, progress: 85 },
+  { rank: "AAA", subtitle: "Commit Pro",      min: 500,  progress: 70 },
+  { rank: "AA",  subtitle: "Hyper Committer", min: 300,  progress: 55 },
+  { rank: "A",   subtitle: "Good Committer",  min: 100,  progress: 40 },
+  { rank: "B",   subtitle: "Middle Committer",min: 50,   progress: 15 },
+  { rank: "C",   subtitle: "Getting Started", min: 1,    progress: 5 },
 ];
