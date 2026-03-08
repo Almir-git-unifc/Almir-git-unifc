d8761b6adeadbb109b3e1d7d1c10a553a3ed3747
Almir-Git <81040770+Almir-git-unifc@users.noreply.github.com>
2026-03-08 16:15:43 -0300

    Add statsCard function to generate SVG stats
    Create a statsCard.js file for statistics cards.

---
+++ scripts/statsCard.js
@@ -0,0 +1,46 @@
+export function statsCard(data){
+
+const {
+user,
+stars,
+commits,
+prs,
+issues,
+contributed,
+rank
+}=data
+
+return `
+<svg width="600" height="220" xmlns="http://www.w3.org/2000/svg">
+
+<rect
+x="0"
+y="0"
+width="600"
+height="220"
+rx="10"
+fill="none"
+stroke="white"
+/>
+
+<text x="30" y="40"
+font-size="20"
+fill="#f7f7f8"
+font-family="Arial"
+>
+${user} GitHub Stats
+</text>
+
+<g fill="#66d1a1" font-size="15">
+
+<text x="60" y="80">Total Stars Earned: ${stars}</text>
+<text x="60" y="105">Total Commits (last year): ${commits}</text>
+<text x="60" y="130">Total PRs: ${prs}</text>
+<text x="60" y="155">Total Issues: ${issues}</text>
+<text x="60" y="180">Contributed to (last year): ${contributed}</text>
+
+</g>
+
+</svg>
+`
+}
