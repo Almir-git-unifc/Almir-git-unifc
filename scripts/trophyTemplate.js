f3e887825cb0f9ccebed42c4d7b128a27c977a32
Almir-Git <81040770+Almir-git-unifc@users.noreply.github.com>
2026-07-25 11:19:23 -0300

    Update trophyTemplate.js
    Line 15 include color <svg width="200" height="150" fill="#315e7f" viewBox="0 0 200 150" xmlns="http://www.w3.org/2000/svg">

--- scripts/trophyTemplate.js
+++ scripts/trophyTemplate.js
@@ -12,7 +12,7 @@ export function trophySVG({
     SSS: "#8856e4", SS: "#8856e4", S: "#8856e4", AAA: "#8856e4", AA: "#8856e4", A: "#8856e4", B: "#8856e4", C: "#8856e4",    
 };
   return `
-<svg width="200" height="150" viewBox="0 0 200 150" xmlns="http://www.w3.org/2000/svg">
+<svg width="200" height="150" fill="#315e7f" viewBox="0 0 200 150" xmlns="http://www.w3.org/2000/svg">
 
 <rect width="200" height="150" rx="12" fill="#315e7f" stroke="#1b3c55"/>
 
