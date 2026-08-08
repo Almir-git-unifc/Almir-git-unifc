824eccaff55a54ba38b099bbd356c1985ae1c750
Almir-Git <81040770+Almir-git-unifc@users.noreply.github.com>
2026-08-08 20:41:52 -0300

    Update rankIcons.js
    Change Icons; AAA = Star, AA = King, A = tropheu, other equals.

--- scripts/rankIcons.js
+++ scripts/rankIcons.js
@@ -9,15 +9,15 @@ export const RANK_ICONS = {
        </svg>`,
   A: `<svg width="24" height="24" viewBox="0 0 24 24" fill="#355fb8" xmlns="http://www.w3.org/2000/svg">
          <circle cx="12" cy="12" r="10" stroke="black" stroke-width="1"/>
-         <text x="12" y="16" text-anchor="middle" font-size="15" fill="#000">⭐</text>
+         <text x="12" y="16" text-anchor="middle" font-size="15" fill="#000">🏆</text>
        </svg>`,
   AA: `<svg width="24" height="24" viewBox="0 0 24 24" fill="#355fb8" xmlns="http://www.w3.org/2000/svg">
          <circle cx="12" cy="12" r="10" stroke="black" stroke-width="1"/>
-         <text x="12" y="16" text-anchor="middle" font-size="12" fill="#000">🏆</text>
+         <text x="12" y="16" text-anchor="middle" font-size="12" fill="#000">👑</text>
        </svg>`,
   AAA: `<svg width="24" height="24" viewBox="0 0 24 24" fill="#355fb8" xmlns="http://www.w3.org/2000/svg">
          <circle cx="12" cy="12" r="10" stroke="black" stroke-width="1"/>
-         <text x="12" y="16" text-anchor="middle" font-size="12" fill="#000">👑</text>
+         <text x="12" y="16" text-anchor="middle" font-size="12" fill="#000">⭐</text>
        </svg>`,
   S: `<svg width="24" height="24" viewBox="0 0 24 24" fill="#FFD700" xmlns="http://www.w3.org/2000/svg">
          <circle cx="12" cy="12" r="10" stroke="black" stroke-width="1"/>
